68bbe490cb9ba71643044e91646b57959defdf04
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-11-04 16:56:32 +0300

    task

--- task.js
+++ task.js
@@ -1,8 +1,43 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  const maxCacheValueCount = 5;
+
+  return (...args) => {
+    const hash = args.join(",");
+    let objectFromCache = cache.find((item) => item.hash === hash);
+    if (objectFromCache) {
+      console.log("Из кэша: " + objectFromCache.value);
+      return "Из кэша: " + objectFromCache.value;
+    }
+
+    const value = func(...args);
+    cache.push({ hash, value });
+    if (cache.length > maxCacheValueCount) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + value);
+    return "Вычисляем: " + value;
+  };
 }
 
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    }
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      wrapper.count++;
+      func(...args);
+    }, delay);
+  }
+  return wrapper;
 }
